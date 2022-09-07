58af9e59f2879cc92fed2ba7cf1024a6a98607ff
James-Kua <kua.james30@gmail.com>
2022-09-07 19:10:14 +0800

    Store JWT in cookie

--- frontend/src/components/SignupPage.js
+++ frontend/src/components/SignupPage.js
@@ -25,7 +25,7 @@ function SignupPage() {
 
     const handleSignup = async () => {
         setIsSignupSuccess(false)
-        const res = await axios.post(URL_USER_SVC, { username, password })
+        const res = await axios.post(URL_USER_SVC + "/signup", { username: username, password: password })
             .catch((err) => {
                 if (err.response.status === STATUS_CODE_CONFLICT) {
                     setErrorDialog('This username already exists')
