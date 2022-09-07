6e61729aece484055cedfaf2790149a66353b33a
James-Kua <kua.james30@gmail.com>
2022-09-07 19:10:14 +0800

    Store JWT in cookie

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -3,17 +3,69 @@ import {
   Button,
   TextField,
   Typography,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
 } from "@mui/material";
 import { useState } from "react";
+import { URL_USER_SVC } from "../configs";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoginSuccess, setIsLoginSuccess] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [dialogMsg, setDialogMsg] = useState("");
+
+  const setSuccessDialog = (msg) => {
+    setIsDialogOpen(true);
+    setDialogMsg(msg);
+  };
+
+  const setErrorDialog = (msg) => {
+    setIsDialogOpen(true)
+    setDialogMsg(msg)
+}
+
+  const closeDialog = () => setIsDialogOpen(false);
+
+  const handleLogin = () => {
+    setIsLoginSuccess(false)
+
+    const credentials = { username: username, password: password };
+
+    fetch(URL_USER_SVC + "/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.message === "success!") {
+          setSuccessDialog("Successfully logged in!")
+          setIsLoginSuccess(true)
+          document.cookie = "token=" + res.user.token
+        } else {
+          setErrorDialog("Invalid credentials!")
+        }
+      })
+      .catch((error) => console.log(error))
+  };
 
   return (
-    <div style={{ justifyContent: "center", alignContent: "center", textAlign: "center"}}>
+    <div
+      style={{
+        justifyContent: "center",
+        alignContent: "center",
+        textAlign: "center",
+      }}
+    >
       <h1 style={{ paddingBottom: "50px" }}> Welcome to PeerPrep!</h1>
-      <div style={{ justifyContent: "center", display: "flex"}}>
+      <div style={{ justifyContent: "center", display: "flex" }}>
         <Box display={"flex"} flexDirection={"column"} width={"30%"}>
           <Typography variant={"h4"} marginBottom={"2rem"}>
             <strong>Login</strong>
@@ -34,9 +86,31 @@ const LoginPage = () => {
             onChange={(e) => setPassword(e.target.value)}
             sx={{ marginBottom: "2rem" }}
           />
-          <Box display={"flex"} flexDirection={"row"} justifyContent={"flex-end"}>
-            <Button variant={"contained"} size={"large"}>Login</Button>
+          <Box
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"flex-end"}
+          >
+            <Button onClick={handleLogin} variant={"contained"} size={"large"}>
+              Login
+            </Button>
           </Box>
+          <Dialog open={isDialogOpen} onClose={closeDialog}>
+            <DialogContent sx={{ textTransform: "capitalize" }}>
+              <DialogContentText sx={{ textAlign: "center", fontWeight: "bold" }}>{dialogMsg}</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              {isLoginSuccess ? (
+                <Button component={Link} to="/home">
+                  Proceed to await match!
+                </Button>
+              ) : (
+                <Button component={Link} to="/signup">
+                  Click here to sign up!
+                </Button>
+              )}
+            </DialogActions>
+          </Dialog>
         </Box>
       </div>
     </div>
