2225dea9f63ba46e4440b8623bea23b23d5f5e01
El0hime <dillontkw@gmail.com>
2022-09-07 17:28:14 +0800

    install matching-service dependencies

--- user-service/index.js
+++ user-service/index.js
@@ -17,7 +17,11 @@ router.post('/login', login)
 router.put('/update-password', verifyToken, updatePassword)
 router.delete('/delete', verifyToken, deleteUser)
  
-
+app.get("/health-check", (req, res) => {
+    res.json({
+        status:"ok",
+    });
+});
 
 app.use('/api/user', router).all((_, res) => {
     res.setHeader('content-type', 'application/json')
