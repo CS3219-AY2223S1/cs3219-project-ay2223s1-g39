47c648dfadadf29f3c13e9cbb94e02f7969a4956
El0hime <dillontkw@gmail.com>
2022-09-08 23:52:29 +0800

    Update repository.js

--- matching-service/model/repository.js
+++ matching-service/model/repository.js
@@ -1,8 +1,6 @@
 import matchModel from './match-model.js';
 import 'dotenv/config'
-//Set up mongoose connection
 import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken'
 
 let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 
