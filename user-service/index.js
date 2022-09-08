2545947a2ac359a881540e05ddd20820260d50f7
El0hime <dillontkw@gmail.com>
2022-09-08 22:46:12 +0800

    Revert "install matching-service dependencies"
    This reverts commit 2225dea9f63ba46e4440b8623bea23b23d5f5e01.

--- user-service/index.js
+++ user-service/index.js
@@ -17,11 +17,7 @@ router.post('/login', login)
 router.put('/update-password', verifyToken, updatePassword)
 router.delete('/delete', verifyToken, deleteUser)
  
-app.get("/health-check", (req, res) => {
-    res.json({
-        status:"ok",
-    });
-});
+
 
 app.use('/api/user', router).all((_, res) => {
     res.setHeader('content-type', 'application/json')
