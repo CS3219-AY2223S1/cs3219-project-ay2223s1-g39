67eb5e096fdac573576cfded927e0d72206e5cbe
boonhaii <boonzxc@gmail.com>
2022-09-10 18:01:58 +0800

    Clicking on button now displays timer page and creates match instance in db

--- matching-service/index.js
+++ matching-service/index.js
@@ -1,30 +1,45 @@
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { ormCreateMatch as createMatch, ormDeleteMatch as deleteMatch} from './service/match-orm.js'
 
 const app = express();
+const server = createServer(app);
+
+app.use(cors()) // config cors so that front-end can use
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
-import { createMatch, deleteMatch } from './controller/match-controller.js';
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Link for frontend client
+    methods: ["GET", "POST", "DELETE"]
+  }
+});
+
 const router = express.Router()
 
 // Controller will contain all the User-defined Routes
-router.get('/', (_, res) => res.send('Hello World from matching-service'))
-router.post('/signup', createMatch)
-router.delete('/delete', deleteMatch)
+router.get('/', (_, res) => res.send("Hello world from matching-service!"));
+router.post('/', (req, res) => createMatch(req, res));
+router.delete('/', (req, res) => createMatch(req, res));
 
-app.use('/api/user', router).all((_, res) => {
+app.use('/api/match', router).all((_, res) => {
     res.setHeader('content-type', 'application/json')
     res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World from matching-service');
-// });
+io.on('connection', (socket) => {
+  console.log(`A client connected, socketID: ${socket.id}`);
 
-// const httpServer = createServer(app)
+  socket.on('createMatch', (data) => {
+    console.log("Creating Match!");
+    const {userOne, difficulty} = data;
+    createMatch(userOne, 'testUser', difficulty, 'testQuestion'); // Cannot pass empty string for null for non-existent values.
+    socket.emit('pendingMatch');
+  })
+})
 
-// httpServer.listen(8001);
-app.listen(8001, () => console.log('user-service listening on port 8001'));
+server.listen(8001, () => console.log('match-service listening on port 8001'));
