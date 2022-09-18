092b0348ea19ce7aeb441a276f66f389b725e6a5
boonhaii <boonzxc@gmail.com>
2022-09-18 20:20:22 +0800

    Add logout button that clears localStorage and cookie

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { createUseStyles } from 'react-jss';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import {
   Button,
 } from "@mui/material";
 import difficulties from '../utils/difficulties';
 
 const useStyles = createUseStyles({
+  headerContent: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
   difficultyButton: {
     margin: "0px 10px"
   },
@@ -49,6 +54,13 @@ const HomePage = () => {
   const [selectedDifficulty, setSelectedDifficulty] = useState('');
   const [user, setUser] = useState(localStorage.getItem('username') || 'User');
 
+  const handleLogout = () => {
+    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;" // Force token to expire.
+    localStorage.removeItem('username');
+    localStorage.removeItem('id');
+    navigate('/login');
+  }
+
   const handleDifficultySelection = (difficulty) => {
     setSelectedDifficulty(difficulty);
   }
@@ -69,8 +81,20 @@ const HomePage = () => {
 
   return (
     <div> 
-      <h2>Welcome back {user}!</h2>
-      <h4>Back for another grind?</h4>
+      <div className={classes.headerContent}>
+        <div>
+          <h2>Welcome back {user}!</h2>
+          <h4>Back for another grind?</h4>
+        </div>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={handleLogout}
+          sx={{ fontWeight: "bold", height: 40 }}
+        >
+          Logout
+        </Button>
+      </div>
       <br/>
       <h3 className={classes.selectionPrompt}>Select your desired difficulty level below:</h3>
       <div className={classes.selectionBoxesContainer}>
