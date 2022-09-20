cb10efc033edbe6edd06d9cded8d71f3ca054556
ChuangZheQuan <zhequan7chuang@gmail.com>
2022-09-20 10:10:54 +0800

    create question service

---
+++ question-service/question-controller.js
@@ -0,0 +1,54 @@
+import { ormCreateQuestion as _createQuestion, ormGetQuestion as _getQuestion, ormGetQuestionsByDifficulty as _getQuestionsByDifficulty } from './question-orm.js'
+
+export async function createQuestion(req, res) {
+    try {
+        
+        const { difficulty, title, question, examples, constraints } = req.body;
+        if (difficulty && question && title) {
+            const resp = await _createQuestion(difficulty, title, question, examples, constraints);
+            if (resp.err) {
+                return res.status(400).json({message: resp.err});
+            } else {
+                return res.status(201).json({message: `Created new question successfully`, question: resp});
+            }
+        } else {
+            return res.status(400).json({message: 'Difficulty and/or question is missing!'});
+        }
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({message: 'Database failure when creating new question! '})
+    }
+}
+
+export async function getQuestion(req, res) {
+    try {
+        const { id } = req.body;
+        const resp = await _getQuestion(id);
+        if (resp == null) {
+            return res.status(404).json({message: 'Question does not exist!'})
+        }
+        if (resp.err) {
+            return res.status(400).json({message: resp.err});
+        }
+        return res.status(200).json({message: 'Got question successfully', question: resp});
+
+    } catch (err) {
+        return res.status(500).json({message: 'Database failure when getting question!'})
+    }
+}
+
+export async function getQuestionsByDifficulty(req, res) {
+    try {
+        const { difficulty } = req.body;
+        if (difficulty != 'easy' || difficulty != 'medium' || difficulty != 'hard') {
+            return res.status(400).json({message: "input invalid difficulty!"})
+        }
+        const resp = await _getQuestionsByDifficulty(difficulty);
+        if (resp.err) {
+            return res.status(400).json({message: resp.err});
+        }
+        return res.status(200).json({message: 'Got questions successfully', question: resp});
+    } catch (err) {
+        return res.status(500).json({message: 'Database failure when getting questions!'})
+    }
+}
---
+++ question-service/question-orm.js
@@ -0,0 +1,30 @@
+import { createQuestion, getQuestion, getQuestionsByDifficulty } from './question-service.js';
+
+//need to separate orm functions from repository to decouple business logic from persistence
+export async function ormCreateQuestion(difficulty, title, question, examples, constraints) {
+    try {
+        console.log(constraints);
+        const newQuestion = await createQuestion({difficulty: difficulty, title: title, question: question, examples: examples, constraints: constraints});
+        return newQuestion;
+    } catch (err) {
+        return { err: err };
+    }
+}
+
+export async function ormGetQuestion(id) {
+    try {
+        const question = await getQuestion({id: id})
+        return question
+    } catch (err) {
+        return {err: err}
+    }
+}
+
+export async function ormGetQuestionsByDifficulty(difficulty) {
+    try {
+        const questions = await getQuestionsByDifficulty({difficulty: difficulty});
+        return questions
+    } catch (err) {
+        return {err: err}
+    }
+}
