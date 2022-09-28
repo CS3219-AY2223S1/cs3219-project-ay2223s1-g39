69cf5d3bd559e2da8da0afd743a14bd8dc47bca5
El0hime <dillontkw@gmail.com>
2022-09-29 02:46:04 +0800

    Setup default user for login in user service test

--- user-service/test/test.js
+++ user-service/test/test.js
@@ -4,25 +4,23 @@ import chaiHttp from "chai-http";
 import index from "../index.js";
 const expect = chai.expect;
 let should = chai.should();
-let defaultUser = {
-  username: "admin0",
-  password: "admin0123"
-};
-
-let token;
 
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
 
-
-
 describe("User Service", function () {
-
-  beforeEach(done => {
+  let defaultUser = {
+    username: "admin0",
+    password: "admin0123",
+  };
+  
+  let token = "";
+  before("Signup user", (done) => {
     chai
       .request(index)
       .post("/api/user/signup")
+      .set("Accept", "application/json")
       .send(defaultUser)
       .end((err, res) => {
         // res.should.have.status(200);
@@ -30,39 +28,30 @@ describe("User Service", function () {
       });
   });
 
-  // before('Login user', async () => {
-  //   const response = await chai.request(server)
-  //     .post('api/v1/account')
-  //     .set('Accept', 'application/json')
-  //     .send({ "email": "john@gmail.com", "password": "123456" })
-  //   token = res.body.token;
-  // });
-  beforeEach(done => {
+  before("Login user", async () => {
+    const response = await chai
+      .request(index)
+      .post("/api/user/login")
+      .set("Accept", "application/json")
+      .send(defaultUser);
+    token = response.body.user.token;
+  });
+
+  it("should delete user successfully", (done) => {
     chai
       .request(index)
-      .post("api/user/login")
-      .send(defaultUser)
+      .delete("/api/user/delete")
+      .send({
+        username: "admin0",
+        password: "admin0123",
+        token: token
+      })
+      .set("x-auth-token", token)
+      .auth(token, { type: "bearer" })
       .end((err, res) => {
-        token = res.token;
-        res.should.have.status(200);
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a("object");
         done();
       });
   });
-
-  describe("Delete test user", () => {
-    it("should delete user successfully", (done) => {
-      chai
-        .request(index)
-        .delete("api/user/delete")
-        .send(defaultUser)
-        .end((err, res) => {
-          console.log(err)
-          res.should.have.status(200);
-          res.body.should.be.a("object");
-          done();
-        });
-    });
-  });
-
-
 });
