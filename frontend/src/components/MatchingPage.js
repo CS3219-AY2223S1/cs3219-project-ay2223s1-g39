3f6314903bda49f9d39d0209885902c7cc6bcb3a
boonhaii <boonzxc@gmail.com>
2022-10-01 00:20:32 +0800

    Complete frontend for matchingPage, update matchingService/page to allow for matching cancellations

--- frontend/src/components/MatchingPage.js
+++ frontend/src/components/MatchingPage.js
@@ -1,17 +1,90 @@
-import React from 'react';
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import React, { useState, useEffect } from 'react';
+import { createUseStyles } from 'react-jss';
+import { Button, Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText } from "@mui/material";
+import {HashLoader} from "react-spinners";
 import { io } from 'socket.io-client';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation, Link } from 'react-router-dom';
 
 const socket = io.connect("http://localhost:8001");
+const loadingMessages = [
+  "Following a trail to find your coding partner...",
+  "Searching far and wide, to find the one that's right...",
+  "Hang on, we're finding you a buddy...",
+  "Finding you a buddy, so just sit back and relax..."
+]
+const chosenMessage = loadingMessages[Math.floor(Math.random() * loadingMessages.length)];
+
+const useStyles = createUseStyles({
+  matchingPage: {
+    display: "flex",
+    height: "100%",
+    alignContent: "center",
+    textAlign: "center", 
+    backgroundColor: "#b5dce9",
+    flexDirection: "column",
+  },
+  loader: {
+    display: "block",
+    marginTop: "10%",
+    marginBottom: "10px",
+    margin: "auto",
+    height: "fit-content"
+  },
+  loadingMessage: {
+    display: "block",
+    color: "white",
+    fontSize: "30px",
+    fontWeight: "bold",
+    height: "fit-content"
+  },
+  timeRemaining: {
+    display: "block",
+    color: "white",
+    fontSize: "20px",
+    marginBottom: "30px"
+  },
+  matchFailureNotif: {
+    display: "block",
+    color: "white",
+    fontSize: "30px",
+  },
+  exitDialogContainer: {
+    borderRadius: "10px",
+    boxShadow: "box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px"
+  }, 
+  exitDialogOptions: {
+    textTransform: "none",
+    fontWeight: "bold",
+    margin: "5px",
+    height: "30px"
+  }
+})
 
 const MatchingPage = () => {
     const navigate = useNavigate();
+    const classes = useStyles();
     const { state } = useLocation();
     const { username, difficulty } = state;
     const token = document.cookie.substring(6);
+    const [timeLeft, setTimeLeft] = useState(30);
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [isFailure, setIsFailure] = useState(false);
 
-    socket.emit("createPendingMatch", {username: username, token: token, difficulty: difficulty});
+    useEffect(() => {
+      if (timeLeft === 30) {
+        socket.emit("createPendingMatch", {username: username, token: token, difficulty: difficulty});
+      } 
+      
+      if (timeLeft > 0) {
+        const newTimeLeft = timeLeft - 1;
+        setTimeout(() => setTimeLeft(newTimeLeft), 1000);
+      } else {
+        setIsFailure(true);
+      }
+    })
 
     socket.on("matchSuccess", (data) => {
       clearTimeout();
@@ -28,24 +101,59 @@ const MatchingPage = () => {
     socket.on("matchFailure", () => {
       return (navigate('/home'));
     })
+
+    const openDialog = () => setIsDialogOpen(true);
+
+    const closeDialog = () => setIsDialogOpen(false);
+
+    const cancelMatching = () => {
+      socket.emit("cancelMatch", {difficulty: difficulty});
+      return navigate("/home");
+    }
     
     return (
-      <div style={{ justifyContent: "center", alignContent: "center", textAlign: "center"}}>
-        <h2 style={{ paddingBottom: "35px" }}>Currently matching you with another person!</h2>
-        <div style={{ justifyContent: "center", display: "flex"}}>
-        <CountdownCircleTimer
-          isPlaying
-          duration={30}
-          size={300}
-          colors={["#42f57b", "#F7B801", "#A30000", "#A30000"]}
-          colorsTime={[15, 10, 5, 0]}
-          onComplete={() => ({
-            shouldRepeat: false,
-          })}
-        >
-          {({ remainingTime }) => <h1>{remainingTime === 0 ? <h5>No match found...</h5> : remainingTime}</h1>}
-        </CountdownCircleTimer>
+      <div className={classes.matchingPage}>
+        <div className={classes.loader}>
+          <HashLoader size={300} color={"white"} />
         </div>
+        { !isFailure && 
+          <div>
+            <div className={classes.loadingMessage}>
+              <p>{chosenMessage}</p>
+            </div>
+            <div className={classes.timeRemaining}>
+              {timeLeft} seconds remaining...
+            </div>
+            <Button variant="contained" color="error" onClick={openDialog} sx={{ fontWeight: "bold", height: 40 }}> 
+              Cancel 
+            </Button>
+            <Dialog open={isDialogOpen} onClose={closeDialog} className={classes.exitDialog}>
+            <DialogContent>
+              <DialogContentText sx={{ textAlign: "center", fontWeight: "bold" }}>
+                You are about to exit the matching queue.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={cancelMatching} className={classes.exitDialogOptions}>
+                <p style={{ fontWeight: "bold", color: "red"}}>
+                  Confirm
+                </p>
+              </Button>
+              <Button onClick={closeDialog} className={classes.exitDialogOptions}>
+                <p>
+                  Return
+                </p>
+              </Button>
+            </DialogActions>
+          </Dialog>
+          </div>
+        }
+        { isFailure && 
+          <h4 className={classes.matchFailureNotif}> 
+            No suitable match found! Returning you back to the main page... 
+          </h4>
+        }
+        
       </div>
     )
 };
