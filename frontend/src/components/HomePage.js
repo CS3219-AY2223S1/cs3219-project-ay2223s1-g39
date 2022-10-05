95da9978942a101e15489aafa95f2a06a7024efe
boonhaii <boonzxc@gmail.com>
2022-10-05 18:37:32 +0800

    Complete basic styling for homepage

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createUseStyles } from 'react-jss';
 import { useNavigate, Link } from 'react-router-dom';
 import {
@@ -6,27 +6,22 @@ import {
   Grid
 } from "@mui/material";
 import logo from '../assets/logo.png'
+import footer from '../assets/homePageImage.svg'
 import difficulties from '../utils/difficulties';
 
 const useStyles = createUseStyles({
   homePage: {
-    height: "100vh"
-  },
-  background: {
-    position: "absolute"
-  },
-  greyTriangle: {
-    width: "100%",
-    height: "100%",
-    background: "conic-gradient(at 50% 50%,transparent 135deg,green 0,green 225deg, transparent 0)"
+    height: "100vh",
+    backgroundColor: "#b5dce9"
   },
   banner: {
-    margin: "auto",
+    marginBottom: "20px",
     width: "100%",
     height:"fit-content",
-    backgroundColor: "transparent",
+    backgroundColor: "white",
     justifyContent: "space-between",
     display: "flex",
+    boxShadow: "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px"
   },
   logo: {
     margin: "20px 0 20px 20px",
@@ -43,32 +38,40 @@ const useStyles = createUseStyles({
     margin: "auto",
   },
   welcomePrompt: {
-    margin: "0 20px 20px 20px",
+    margin: "0 20px 40px 20px",
     display: "block",
     padding: "15px 20px 35px 20px",
     borderRadius: "20px",
+    backgroundColor: "white",
     boxShadow: "rgba(0, 0, 0, 0.3) 0px 4px 12px"
   },
   mainContent: {
+    display: "flex",
     width: "100%",
     padding: "0 20px",
-    backgroundColor: "pink",
-    margin: "auto",
+    zIndex: "1"
   },
-  summaryStatistics: {
-    justifyContent:"center",
-    backgroundColor:"pink",
-    textAlign:"center"
+  featureCardContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
   },
-  matchFind: {
-
+  featureCard: {
+    display: "flex",
+    flexDirection: "column",
+    boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+    backgroundColor: "white",
+    width: "400px",
+    height: "400px",
+    padding: "15px 15px",
+    borderRadius: "10px"
+  },
+  featureCardPrompt: {
+    display: "flex",
+    justifyContent: "center",
   },
   difficultyButton: {
     margin: "0px 10px"
-  },
-  selectionPrompt: {
-    display: "flex",
-    justifyContent: "center",
   },
   selectionBoxesContainer: {
     justifyContent: "center",
@@ -77,24 +80,38 @@ const useStyles = createUseStyles({
     margin: "auto",
   },
   selectionBox: {
-    borderBottom: "1px solid black",
+    border: "1px solid #0275d8",
+    borderRadius: "10px",
+    margin: "15px",
     display: "flex",
-    height: "80px",
-    width: "500px",
+    height: "40px",
+    width: "250px",
     alignItems: "center",
     padding: "10px 20px",
-    fontSize: "24px",
+    fontSize: "20px",
     fontWeight: "bold",
+    color: "#0275d8",
     "&:hover": {
-      backgroundColor: "#cccccc",
+      backgroundColor: "#0275d8",
+      color: "white"
     }
   },
   findMatchBtn: {
     display: "flex",
     margin: "20px auto",
+    textTransform: "none"
   }, 
   chosenDifficulty: {
-    backgroundColor: "#cccccc"
+    backgroundColor: "#0275d8",
+    color: "white"
+  },
+  footerDesign: {
+    position: "absolute",
+    height: "500px",
+    width: "500px",
+    bottom: "0",
+    right: "0",
+    zIndex: "0"
   }
 })
 
@@ -104,6 +121,15 @@ const HomePage = () => {
 
   const [selectedDifficulty, setSelectedDifficulty] = useState('');
   const [user, setUser] = useState(localStorage.getItem('username') || 'User');
+  const [quote, setQuote] = useState('');
+
+  // Fetch quote for header card
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => setQuote(data[Math.floor(Math.random() * data.length)]))
+  }, [])
+
 
   const handleLogout = () => {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;" // Force token to expire.
@@ -132,8 +158,8 @@ const HomePage = () => {
 
   const renderMatchFindComponent = () => {
     return (
-      <div>
-        <h3 className={classes.selectionPrompt}>Select your desired difficulty level below:</h3>
+      <div className={classes.featureCard}>
+        <h2 className={classes.featureCardPrompt}>Up for another round?</h2>
         <div className={classes.selectionBoxesContainer}>
           {Object.keys(difficulties).map((difficulty) => <SelectionBox key={difficulty} difficulty={difficulty} />)}
         </div>
@@ -142,19 +168,31 @@ const HomePage = () => {
     )
   }
 
+  const renderSummaryStatisticsComponent = () => {
+    return (
+      <div className={classes.featureCard}>
+        <h2 className={classes.featureCardPrompt}>Summary</h2>
+      </div>
+    )
+  }
+
+  const renderLearningHistoryComponent = () => {
+    return (
+      <div className={classes.featureCard}>
+        <h2 className={classes.featureCardPrompt}>Recent Attempts</h2>
+      </div>
+    )
+  }
+
   return (
     <div className={classes.homePage}>
-      <div className={classes.background}>
-        <div className={classes.greyTriangle}></div>
-      </div>
-      
       <div className={classes.banner}>
         <img src={logo} className={classes.logo}></img>
         <Button
           variant="contained"
           color="error"
           onClick={handleLogout}
-          sx={{ fontWeight: "bold", height: 35 }}
+          sx={{ fontWeight: "bold", height: 40 }}
           className={classes.logoutButton}
         >
           Logout
@@ -163,28 +201,29 @@ const HomePage = () => {
       
       <div className={classes.pageBody}>
         <div className={classes.welcomePrompt}>
-          <h1>Welcome back {user}!</h1>
-          <p><i>Inspiring Quote here. Fetch from some API.</i></p>
+          <h1>Welcome back {user}! 👋</h1>
+          <p><i>{quote.text} - {quote.author ? quote.author : "Unknown"}</i></p>
         </div>
 
         <Grid container className={classes.mainContent}>
-          <Grid item spacing={3} xs={4} sx={{border: "1px solid black"}}>
-            <div className={`${classes.featureCard} ${classes.summaryStatistics}`}>
-              Summary Statistics Component
+          <Grid item xs={4}>
+            <div className={`${classes.featureCardContainer}`}>
+              {renderSummaryStatisticsComponent()}
             </div>
           </Grid>
-          <Grid item spacing={3} xs={4} sx={{border: "1px solid red"}}>
-            <div className={`${classes.featureCard} ${classes.matchFind}`}>
+          <Grid item xs={4}>
+            <div className={`${classes.featureCardContainer}`}>
               {renderMatchFindComponent()}
             </div>
           </Grid>
-          <Grid item spacing={3} xs={4} sx={{border: "1px solid red"}}>
-            <div className={`${classes.featureCard} ${classes.historyService}`}>
-              Learning History Service
+          <Grid item xs={4}>
+            <div className={`${classes.featureCardContainer}`}>
+              {renderLearningHistoryComponent()}
             </div>
           </Grid>
         </Grid>
       </div>
+      {/*<img className={classes.footerDesign} src={footer} alt="footer-img"></img> */}
     </div>
   )
 }
